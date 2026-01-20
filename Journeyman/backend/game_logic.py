ceba9f33e6b3e7df32136b05ac4cf568daf5ce8e
Daniel Jang <danjang815@gmail.com>
2026-01-19 21:07:10 -0800

    implemented alternative correct answers

--- Journeyman/backend/game_logic.py
+++ Journeyman/backend/game_logic.py
@@ -1,7 +1,16 @@
+def answer_variations(team):
+    aliases = {team.lower()}
+    team_words = team.split()
+    aliases.add(team_words[-1])
+    return aliases
+
 def guess_check(guess, correct_teams, position):
-    if guess == correct_teams[position]:
+
+    potential_answers = [answer_variations(team) for team in correct_teams]
+
+    if guess == correct_teams[position] or potential_answers[position]:
         return "green"
-    elif guess in correct_teams:
+    elif guess in correct_teams or potential_answers:
         return "yellow"
     else:
         return "gray"
