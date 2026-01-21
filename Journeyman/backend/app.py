7fdb8e031b2c93a9b9e0e482fa94534e06efacfc
Daniel Jang <danjang815@gmail.com>
2026-01-20 21:49:47 -0800

    added guess checking route for flask

--- Journeyman/backend/app.py
+++ Journeyman/backend/app.py
@@ -11,10 +11,24 @@ def home():
 @app.route("/new-game")
 def new_game():
     player_name, teams = randomPlayer()
-    return {
+    return jsonify ({
         "Player" : player_name,
         "Number of Teams": len(teams)
-    }
+    })
+
+@app.route("/check-guess", methods = ["POST"])
+def check_guess():
+    player_data = request.json
+    guess = player_data.get("guess")
+    correct_teams = player_data.get("teams")
+    position = player_data.get("position")
+
+    result = guess_check(guess, correct_teams, position)
+
+    return jsonify ({
+        "result": result
+    })
+
 
 if __name__ == "__main__":
     app.run(debug = True)
