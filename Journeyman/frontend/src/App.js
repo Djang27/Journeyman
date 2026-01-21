24b9f51eeb6a264a33e0ec98984052b3c1bc7725
Daniel Jang <danjang815@gmail.com>
2026-01-20 23:04:17 -0800

    connected react frontend with flask backend /new-game endpoint

--- Journeyman/frontend/src/App.js
+++ Journeyman/frontend/src/App.js
@@ -1,6 +1,17 @@
 import React, {useState, useEffect} from 'react'
 
 function App() {
+    const [player, set_player] = useState("")
+    useEffect(() => {
+        fetch("/new-game").then(
+            (res) => res.json()
+        ).then(
+            data => {
+                set_player(data.Player)
+                console.log(data.Player)
+            }
+        )
+    }, [])
     return(
       <div>
 
