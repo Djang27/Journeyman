57332001a11cf034577b780aaf01d7b3c06d7714
Daniel Jang <danjang815@gmail.com>
2026-03-11 13:50:07 -0700

    added win message and implemented play again button

--- Journeyman/frontend/src/components/game.js
+++ Journeyman/frontend/src/components/game.js
@@ -1,6 +1,6 @@
 import TeamList from "./team_list"
 
-function GameScreen({player, teams, guesses, results, on_guess_change, on_submit}) {
+function GameScreen({player, teams, guesses, results, on_guess_change, on_submit, has_won, on_play_again}) {
     return (
         <div>
             <h2>Player: {player}</h2>
@@ -9,7 +9,14 @@ function GameScreen({player, teams, guesses, results, on_guess_change, on_submit
             guesses = {guesses}
             results = {results}
             on_guess_change = {on_guess_change}
-            on_submit = {on_submit} />
+            on_submit = {on_submit} 
+            />
+            {has_won && (
+                <div>
+                    <h2>You Win! </h2>
+                    <button onClick = {on_play_again}>Play Again</button>
+                </div>
+            )}
         </div>
     )
 }
